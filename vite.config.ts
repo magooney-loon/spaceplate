aef51b83258c42f64788a29318ff12739fea6bfe
Magooney <magooney-loon@proton.me>
2026-03-09 12:30:17 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { threlteStudio } from "@threlte/studio/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    threlteStudio(),
+    svelte(),
+    visualizer({
+      filename: "stats.html",
+      open: false,
+      gzipSize: true,
+    }),
+  ],
 });
