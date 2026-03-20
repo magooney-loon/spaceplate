083c5e0a965257e9c170bba9a4d2c9f9dde3e1c1
Magooney <magooney-loon@proton.me>
2026-03-20 01:08:05 +0100

    Update MainMenuHud.svelte

--- src/scenes/MainMenuHud.svelte
+++ src/scenes/MainMenuHud.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { sceneActions } from '$extensions/scene/scene.svelte';
 	import { soundActions } from '$core/GlobalAudio.svelte';
+	import { BASE_URL } from '$extensions/settings/settings.svelte';
 	import SettingsHud from '$scenes/SettingsHud.svelte';
 
 	let showSettings = $state(false);
@@ -11,6 +12,7 @@
 	<div class="pointer-events-auto">
 		<!-- Menu Title -->
 		<div class="absolute top-[20%] left-1/2 -translate-x-1/2 text-center">
+			<img src="{BASE_URL}favicon.ico" alt="Logo" class="w-20 h-20 mx-auto mb-4" />
 			<h1 class="text-5xl text-white font-bold m-0" style="text-shadow: 0 0 20px #4a90d9;">
 				SPACEPLATE ENGINE
 			</h1>
