b6a6344c80665a03d8b5ad7af6aed43415d751b5
Magooney <magooney-loon@proton.me>
2026-03-21 21:45:14 +0100

    Update DemoFloor.svelte

--- src/scenes/DemoFloor.svelte
+++ src/scenes/DemoFloor.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
-	import { AutoColliders, RigidBody } from '@threlte/rapier';
+	import { Collider } from '@threlte/rapier';
 </script>
 
-<T.Group position={[0, -0.5, 0]} userData={{ selectable: false, hideInTree: true }}>
-	<RigidBody type="fixed" userData={{ selectable: false, hideInTree: true }}>
-		<T.Group userData={{ selectable: false, hideInTree: true }}>
-			<AutoColliders shape="cuboid">
-				<T.Mesh receiveShadow>
-					<T.BoxGeometry args={[20, 0.001, 20]} />
-					<T.MeshStandardMaterial color="gray" />
-				</T.Mesh>
-			</AutoColliders>
-		</T.Group>
-	</RigidBody>
+<T.Group userData={{ selectable: false, hideInTree: true }}>
+	<Collider shape="cuboid" args={[10, 0.5, 10]} />
+	<T.Mesh position={[0, 0.5, 0]} receiveShadow userData={{ selectable: false, hideInTree: true }}>
+		<T.BoxGeometry args={[20, 0.001, 20]} />
+		<T.MeshStandardMaterial color="gray" />
+	</T.Mesh>
 </T.Group>
