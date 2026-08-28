c6de839951ce26bfc9cfa7c5eca65e9710d7f0ad
Magooney <magooney-loon@proton.me>
2026-08-28 10:48:14 +0200

    Update DemoFloor.svelte

--- src/scenes/DemoScene/DemoFloor.svelte
+++ src/scenes/DemoScene/DemoFloor.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <T.Group userData={{ selectable: false, hideInTree: true }}>
-	<Collider shape="cuboid" args={[10, 0.5, 10]} />
-	<T.Mesh position={[0, 0.5, 0]} receiveShadow userData={{ selectable: false, hideInTree: true }}>
+	<Collider shape="cuboid" args={[10, 0, 10]} />
+	<T.Mesh position={[0, 0, 0]} receiveShadow userData={{ selectable: false, hideInTree: true }}>
 		<T.BoxGeometry args={[20, 0.001, 20]} />
 		<T.MeshStandardMaterial color="gray" />
 	</T.Mesh>
