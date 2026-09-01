3fdf111b98282f54169bd77c00d2d3f44a61ee26
Magooney <magooney-loon@proton.me>
2026-09-01 19:28:46 +0200

    clean

--- src/Scene.svelte
+++ src/Scene.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
 	import { Skybox, Camera, GlobalAudio } from '$core';
-	import { T, useTask } from '@threlte/core/webgpu';
-	import { physicsActions } from '$extensions/physics';
+	import { T } from '@threlte/core/webgpu';
 	import { sceneState } from '$extensions/scene';
 	import MainMenu from '$scenes/MainMenu/MainMenu.svelte';
 	import DemoScene from '$scenes/DemoScene/DemoScene.svelte';
 
-	let prevScene = $state(sceneState.currentScene);
-
-	useTask(() => {
-		if (sceneState.currentScene !== prevScene) {
-			if (prevScene === 'demoScene' && sceneState.currentScene !== 'demoScene') {
-				physicsActions.clearBodies();
-			}
-			prevScene = sceneState.currentScene;
-		}
-	});
 </script>
 
 <Camera />
