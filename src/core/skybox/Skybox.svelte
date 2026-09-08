4f6e58e9ce78ef7a2627e9464bc549535f3ac884
Magooney <magooney-loon@proton.me>
2026-09-08 20:35:51 +0200

    ignore birds for now idk

--- src/core/skybox/Skybox.svelte
+++ src/core/skybox/Skybox.svelte
@@ -8,7 +8,7 @@
 	import Nebula from './layers/celestial/Nebula.svelte';
 	import Stars from './layers/celestial/Stars.svelte';
 	import Meteors from './layers/celestial/Meteors.svelte';
-	import Birds from './layers/fauna/Birds.svelte';
+/* 	import Birds from './layers/fauna/Birds.svelte'; */
 	import Rain from './layers/precipitation/Rain.svelte';
 	import Snow from './layers/precipitation/Snow.svelte';
 	import LensDriver from './layers/precipitation/LensDriver.svelte';
@@ -144,7 +144,7 @@
 		<Stars radius={1000} />
 		<Meteors radius={1000} />
 		<Moon radius={1000} />
-		<Birds />
+		<!-- <Birds /> -->
 		<Lightning />
 		<CloudDeck radius={1000} />
 		<!-- Remounted on preset change: counts are baked at mount (see PRECIPITATION).
